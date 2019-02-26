1e0ee2421bb3e4d37046f25918c3bc4ec618af1d
Hcupcdown <370188194@qq.com>
2019-02-26 21:54:54 +0800

    lenet5网络实现

---
+++ mnist_CNN/mnist_CNN_backward.py
@@ -0,0 +1,81 @@
+#creatd by cup&cdown
+#2019-2-26
+#Lenet5反向传播训练
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist_CNN_forward
+import os
+import numpy as np
+
+BATCH_SIZE = 100
+LENARNING_RATE_BASE = 0.005
+LENARNING_RATE_DECAY= 0.99
+REGULARIZER = 0.0001
+STEPS = 50000
+MOVING_AVERAGE_DECAY = 0.99
+MODE_SAVE_PATH = "./mode"
+MODE_NAME="mnist_model"
+
+def backward(mnist):
+
+    x=tf.placeholder(tf.float32,[BATCH_SIZE, mnist_CNN_forward.INPUT_SIZE,mnist_CNN_forward.INPUT_SIZE,mnist_CNN_forward.NUM_CHANNELS])
+    y_ = tf.placeholder(tf.float32,[None, mnist_CNN_forward.OUTPUT_NODE])
+    y = mnist_CNN_forward.forward(x,True,REGULARIZER)
+    global_step = tf.Variable(0,trainable=False)
+    
+    #损失函数，交叉熵
+    ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_,1))
+    cem =tf.reduce_mean(ce)
+    loss = cem + tf.add_n(tf.get_collection('losses'))
+    
+    #指数衰减学习率
+    learning_rate = tf.train.exponential_decay(
+        LENARNING_RATE_BASE,
+        global_step,
+        mnist.train.num_examples / BATCH_SIZE,
+        LENARNING_RATE_DECAY,
+        staircase=True
+    )
+
+    #滑动平均优化,MOVING_AVERAGE_DECAY为平均衰减率
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    
+    #trainable_variables将待训练参数汇总为列表
+    #ema.apply 将对括号内所有参数求滑动平均
+    ema_op=ema.apply(tf.trainable_variables())
+
+    #control_dependencies将train_step和ema_op绑定，使两者同步运行
+    with tf.control_dependencies([train_step, ema_op]):
+        train_op = tf.no_op(name='train')
+
+    #保存模型
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+
+        #实现断点续训
+        ckpt = tf.train.get_checkpoint_state(MODE_SAVE_PATH)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+
+        for i in range(STEPS):
+            xs, ys =mnist.train.next_batch(BATCH_SIZE)
+            reshape_xs = np.reshape(xs,(
+                BATCH_SIZE,
+                mnist_CNN_forward.INPUT_SIZE,
+                mnist_CNN_forward.INPUT_SIZE,
+                mnist_CNN_forward.NUM_CHANNELS))
+            _, loss_value,step =sess.run([train_op, loss, global_step], feed_dict={x: reshape_xs, y_: ys})
+            if i % 1000 ==0:
+                print("after %d training steps, loss on traning batch is %g" % (step, loss_value))
+                saver.save(sess,os.path.join(MODE_SAVE_PATH,MODE_NAME), global_step=global_step)
+
+def main():
+    mnist = input_data.read_data_sets("./data/",one_hot=True)
+    backward(mnist)
+
+if __name__ == '__main__':
+    main()
---
+++ mnist_CNN/mnist_CNN_forward.py
@@ -0,0 +1,70 @@
+#creatd by cup&cdown
+#2019-2-26
+#Lenet5定义前向传播节点
+import tensorflow as tf
+
+#定义节点个数
+INPUT_SIZE = 28
+NUM_CHANNELS = 1
+CONV1_SIZE = 5
+#第一层卷积核个数
+CONV1_KERNEL_NUM = 32
+CONV2_SIZE = 5
+CONV2_KERNEL_NUM = 64
+#全连接层的节点个数
+FC_SIZE = 512
+OUTPUT_NODE = 10
+
+def get_weight(shape, regularizer):
+    #过滤掉偏离较大的正态分布
+    w = tf.Variable(tf.truncated_normal(shape,stddev = 0.1))
+    #是否正则化
+    if regularizer != None:
+        tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(regularizer)(w))
+    return w
+
+def get_bias(shape):
+    b = tf.Variable(tf.zeros(shape))
+    return b
+
+#卷积操作
+def conv2d(x, w):
+    #conv2d参数为：输入描述（batch，行分辨率，列分辨率，通道数），卷积核描述，滑动步长
+    return tf.nn.conv2d(x , w,strides=[1,1,1,1],padding="SAME")
+
+#最大池化
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1],strides=[1,2,2,1],padding = "SAME")
+
+def forward(x, train,regularizer):
+    #第一卷积层
+    conv1_w = get_weight([CONV1_SIZE,CONV1_SIZE,NUM_CHANNELS,CONV1_KERNEL_NUM],regularizer)
+    conv1_b =get_bias([CONV1_KERNEL_NUM])
+    conv1=conv2d(x, conv1_w)
+    #激活函数relu
+    relu1 = tf.nn.relu(tf.nn.bias_add(conv1,conv1_b))
+    pool1 = max_pool_2x2(relu1)
+
+    #第二层卷积
+    conv2_w = get_weight([CONV2_SIZE,CONV1_SIZE,CONV1_KERNEL_NUM,CONV2_KERNEL_NUM],regularizer)
+    conv2_b = get_bias([CONV2_KERNEL_NUM])
+    conv2 = conv2d(pool1, conv2_w)
+    relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_b))
+    pool2 = max_pool_2x2(relu2)
+
+    #将pool2的shape存为list
+    pool_shape = pool2.get_shape().as_list()
+    nodes = pool_shape[1]*pool_shape[2]*pool_shape[3]
+    reshape = tf.reshape(pool2, [pool_shape[0],nodes])
+
+    fc1_w = get_weight([nodes, FC_SIZE], regularizer)
+    fc1_b = get_bias([FC_SIZE])
+    fc1 = tf.nn.relu(tf.matmul(reshape, fc1_w) + fc1_b)
+    if train:
+        #在训练过程中随机舍弃一部分参数，避免过拟合，加快训练速度
+        fc1 = tf.nn.dropout(fc1, 0.5)
+    
+    fc2_w =get_weight([FC_SIZE, OUTPUT_NODE],regularizer)
+    fc2_b = get_bias ([OUTPUT_NODE])
+    y = tf.matmul(fc1, fc2_w) + fc2_b
+    return y
